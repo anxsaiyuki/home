636b16640b9b8481939246da4b0f4cb8de2bac38
anxsaiyuki <anxsaiyuki@gmail.com>
2014-04-24 13:32:37 +0800

    added require and message.js

--- project/main.js
+++ project/main.js
@@ -1,14 +1,12 @@
 require.config({
     baseUrl: 'js/lib',
     paths: {
-        jquery: 'jquery-1.11.0.min'
+        jquery: 'jquery-1.11.0.min',
+		message : 'message'
     }
 });
 
-require(['jquery'], function( $ ) {
-    console.log( $ ) // OK
-	$.get( "message.html", function( data ) {
-	  $( "#banner" ).html( data );
-	  console.log( "Load was performed." );
-	});
+require(['message'], function(message) {
+  
+	message.start;
 });
